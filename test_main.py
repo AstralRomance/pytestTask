20f3f431e2019922aa51ca4767d3d2b0a5ae75ad
AstralRomance <andrdoncwork@mail.ru>
2021-01-24 21:57:55 +0300

    debug, test class defined

--- test_main.py
+++ test_main.py
@@ -1,71 +1,79 @@
 import pytest
 import requests
+import itertools
 
-SW_API_BASE_LINK = 'https://swapi.dev/api'
+class TestSWApi:
+    SW_API_BASE_LINK = 'https://swapi.dev/api'
 
-# TASK 1
-@pytest.fixture()
-def get_all_people():
-    request_link = ''.join(SW_API_BASE_LINK,'/people')
-    swapi_request = requests.get(request_link)
-    return swapi_request.json()['results']
+    # TASK 1
+    @pytest.fixture()
+    def get_all_people(self):
+        all_people = []
+        for page in itertools.count(1):
+            swapi_link = ''.join((self.SW_API_BASE_LINK,f'/people/?page={page}'))
+            swapi_request = requests.get(swapi_link)
+            if swapi_request.status_code == 404:
+                break
+            all_people.extend(swapi_request.json()['results'])
+        return all_people
 
-@pytest.fixture()
-def get_different_case_names():
-    return ['LUKE SKYWALKER', 'Luke Skywalker', 'luke skywalker', 'LuKe SkYwAlKeR']
+    @pytest.fixture()
+    def get_different_case_names(self):
+        return ['LUKE SKYWALKER', 'Luke Skywalker', 'luke skywalker', 'LuKe SkYwAlKeR']
 
-#TASK 7
-@pytest.fixture()
-def get_people_object_schema():
-    request_link = ''.join(SW_API_BASE_LINK, '/people/schema')
-    return requests.get(request_link)['required']
+    #TASK 7
+    @pytest.fixture()
+    def get_people_object_schema(self):
+        request_link = ''.join((self.SW_API_BASE_LINK, '/people/schema'))
+        return requests.get(request_link).json()['required']
 
-#TASK 2
-def count_test(get_all_people):
-    request_link = ''.join(SW_API_BASE_LINK,'/people')
-    swapi_request = requests.get(request_link)
-    assert len(get_all_people) == swapi_request.json()['count']
+    #TASK 2
+    def test_count(self, get_all_people):
+        swapi_link = ''.join((self.SW_API_BASE_LINK, f'/people'))
+        num_of_people = requests.get(swapi_link).json()['count']
+        assert len(get_all_people) == num_of_people
 
-#TASK 3
-def unique_names_test(get_all_people):
-    names = [name for name in get_all_people['name']]
-    assert len(names) == len(set(names))
+    #TASK 3
+    def test_unique_names(self, get_all_people):
+        names = [name['name'] for name in get_all_people]
+        print(names)
+        assert len(names) == len(set(names))
 
-#TASK 4
-def case_sensivity_valid_data_test(get_different_case_names):
-    responses = []
-    for name in get_different_case_names:
-        request_link = ''.join(SW_API_BASE_LINK, f'/people/?search={name}')
-        responses.append(requests.get(request_link).status_code)
-    assert all(200==response_code for response_code in responses)
+    #TASK 4
+    def test_case_sensivity_valid_data(self, get_different_case_names):
+        responses = []
+        for name in get_different_case_names:
+            request_link = ''.join((self.SW_API_BASE_LINK, f'/people/?search={name}'))
+            responses.append(requests.get(request_link).status_code)
+        assert all(200==response_code for response_code in responses)
 
-def case_sensivity_correct_data_test(get_different_case_names):
-    correct_name = 'Luke Skywalker'
-    requested_names = []
-    for name in get_different_case_names:
-        request_link = ''.join(SW_API_BASE_LINK, f'/people/?search={name}')
-        requested_namess.append(requests.get(request_link).json()['results']['name'])
-    assert all(correct_name==name for name in requested_name)
+    def test_case_sensivity_correct_data(self, get_different_case_names):
+        correct_name = 'Luke Skywalker'
+        requested_names = []
+        for name in get_different_case_names:
+            request_link = ''.join((self.SW_API_BASE_LINK, f'/people/?search={name}'))
+            requested_names = [i['name'] for i in requests.get(request_link).json()['results']]
+        assert all(correct_name==name for name in requested_names)
 
-#TASK 5
-def zero_page_validate_test():
-    request_link = ''.join(SW_API_BASE_LINK,'/people/?page=0')
-    swapi_request = requests.get(request_link)
-    assert swapi_request.status_code == 404
+    #TASK 5
+    def test_zero_page_validate(self):
+        request_link = ''.join((self.SW_API_BASE_LINK,'/people/?page=0'))
+        swapi_request = requests.get(request_link)
+        assert swapi_request.status_code == 404
 
-#TASK 6
-@pytest.mark.parametrize('name, number',
-                        [('Skywalker', 3),
-                         ('Vader', 1),
-                         ('Darth', 2)])
-def character_number_test(name, number):
-    request_link = ''.join(SW_API_BASE_LINK, f'/people/?search={name}')
-    swapi_request = requests.get(request_link)
-    assert swapi_request.json()['count'] == number
+    #TASK 6
+    @pytest.mark.parametrize('name, number',
+                            [('Skywalker', 3),
+                            ('Vader', 1),
+                            ('Darth', 2)])
+    def test_character_number(self, name, number):
+        request_link = ''.join((self.SW_API_BASE_LINK, f'/people/?search={name}'))
+        swapi_request = requests.get(request_link)
+        assert swapi_request.json()['count'] == number
 
-#TASK 8
-def check_people_schema_test(get_all_people, get_people_object_schema):
-    is_valid = []
-    for people in get_all_people:
-        is_valid.append(all(field in people for field in get_people_object_schema)) 
-    assert all(schema==True for schema in is_valid)
+    #TASK 8
+    def test_check_people_schema(self, get_all_people, get_people_object_schema):
+        is_valid = []
+        for people in get_all_people:
+            is_valid.append(all(field in people for field in get_people_object_schema)) 
+        assert all(schema==True for schema in is_valid)
